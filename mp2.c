3cbec9f186a9d591806f2794673e7f3be3cb1142
Marc <gmarc.teves@gmail.com>
2015-11-29 00:09:45 +0800

    Added record struct

---
+++ mp2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct node {
+  int id;
+  char title[SMS];
+  char artist[SMS];
+  char composer[SMS];
+  char album[SMS];
+  int genre; //0-1-2
+  int rating; //0-5
+  char remarks[SMS];
+  struct node next*;
+} record;
+
+
+
+void main(){
+
+}
